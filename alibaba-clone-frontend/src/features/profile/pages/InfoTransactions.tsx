8e6eddef4c83ca36226e95aa25f5439b4d755f75
AminGh05 <mo.ghoorchian@gmail.com>
2025-07-07 00:07:37 +0330

    feat(charge): added modal and functionality of top-up

--- alibaba-clone-frontend/src/features/profile/pages/InfoTransactions.tsx
+++ alibaba-clone-frontend/src/features/profile/pages/InfoTransactions.tsx
@@ -1,18 +1,24 @@
-import { getMyTransactions, getProfile } from "@/api/features/profileApi";
+import {
+  getMyTransactions,
+  getProfile,
+  topUp,
+} from "@/api/features/profileApi";
 import { useEffect, useState } from "react";
 import TransactionHistory from "../components/TransactionHistory";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
 import { TransactionDto } from "@/shared/models/transaction/TransactionDto";
+import TopUpModal from "../components/TopUpModal";
+import { TopUpDto } from "@/shared/models/transaction/TopUpDto";
 
 const InfoTransactions = () => {
   const [balance, setBalance] = useState<number>(0);
   const [transactions, setTransactions] = useState<TransactionDto[]>([]);
+  const [topUpData, setTopUpData] = useState<TopUpDto | null>(null);
+  const [topUpOpen, setTopUpOpen] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    // fetch balance when the component mounts
+  // fetch balance when the component mounts
     const fetchBalance = async () => {
       try {
         const response = await getProfile();
@@ -34,23 +40,52 @@ const InfoTransactions = () => {
         setLoading(false);
       }
     };
-
+  
+  useEffect(() => {
     fetchBalance();
     fetchTransactions();
   }, []);
+
+  const handleTopUp = async (data: TopUpDto) => {
+    try {
+      const amount: TopUpDto = {
+        amount: data.amount,
+      };
+
+      await topUp(amount);
+      setTopUpData((prev) => (prev ? { ...prev, ...data } : prev));
+      setTopUpOpen(false);
+      // refresh the balance and transactions after topping up
+      await fetchBalance();
+      await fetchTransactions();
+    } catch (error) {
+      console.error("Error topping up:", error);
+      setError("Failed to top up balance.");
+    }
+  };
 
   return (
     <div className="flex flex-col gap-4">
       <div className="bg-white rounded-lg shadow px-6 py-4 min-h-[64px] flex items-center justify-between">
         <div className="text-lg font-semibold text-gray-800 flex items-center">
           Current Balance:{" "}
-          <span className="text-blue-700 ml-1">{balance.toLocaleString()} $</span>
+          <span className="text-blue-700 ml-1">
+            {balance.toLocaleString()} $
+          </span>
         </div>
-        <Button variant="outline" className="w-fit flex items-center h-10">
-          <Link to={"top-up"} className="text-blue-700 font-medium flex items-center h-full">
-            Increase Balance
-          </Link>
+        <Button
+          variant="outline"
+          className="w-fit flex items-center h-10"
+          onClick={() => setTopUpOpen(true)}
+        >
+          Increase Balance
         </Button>
+        <TopUpModal
+          open={topUpOpen}
+          onClose={() => setTopUpOpen(false)}
+          topUp={topUpData}
+          onSave={handleTopUp}
+        />
       </div>
       {loading ? (
         <div className="text-gray-500">Loading...</div>
